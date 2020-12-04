76e58d90a3fbffa67ad527fc08ad4e6fef29a4a9
Artem <dr.artem-k@yandex.ru>
2020-12-05 01:36:12 +0400

    url

--- src/js/config/config.js
+++ src/js/config/config.js
@@ -1,3 +1,3 @@
 export const newsApiServer = NODE_ENV === 'development' ? 'https://newsapi.org/' : 'https://nomoreparties.co/news/';
-export const defaultMainApi = NODE_ENV === 'development' ? 'http://localhost:3000/' : 'https://api.news-explorer-praktikum.ru/';
+export const defaultMainApi = NODE_ENV === 'development' ? 'http://localhost:3000/' : 'https://www.api.news-card.site/';
 // export const defaultMainApi = 'http://localhost:3000/';
